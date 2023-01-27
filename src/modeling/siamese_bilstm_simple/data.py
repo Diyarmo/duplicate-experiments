59c831214d777d1bb366804569b3f11648c573f4
ajawe <diyar.mohammadi@divar.ir>
2023-01-27 03:40:17 +0330

    Add siamese simple bilstm

---
+++ src/modeling/siamese_bilstm_simple/data.py
@@ -0,0 +1,141 @@
+from modeling.siamese_bilstm.data_utils import digit_words, digits
+from data.text_normalizer import normalize_text
+import pandas as pd
+import pytorch_lightning as pl
+import torch
+import torch.multiprocessing
+from torch.utils.data import DataLoader, Dataset
+
+from transformers import PreTrainedTokenizerFast
+
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+
+class DuplicateDataset(Dataset):
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
+        self.original_data = data
+
+    def set_sample_data(self, sample_size):
+        if sample_size >= len(self.original_data):
+            self.data = self.original_data
+        else:
+            self.data = self.original_data.sample(sample_size)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        row = self.data.iloc[item]
+
+        row["post1_title"] = normalize_text(row["post1_title"])
+        row["post2_title"] = normalize_text(row["post2_title"])
+        row["post1_desc"] = normalize_text(row["post1_desc"])
+        row["post2_desc"] = normalize_text(row["post2_desc"])
+
+        # Post 1 - Original
+        post1_title = row["post1_title"]
+        post1_description = row["post1_desc"]
+
+        # Post 2 - Compare
+        post2_title = row["post2_title"]
+        post2_description = row["post2_desc"]
+
+        post1_x = [post1_title, post1_description]
+        post2_x = [post2_title, post2_description]
+        y_duplicate = row["is_duplicate"]
+
+        return post1_x, post2_x, y_duplicate
+
+
+class DuplicateDataLoader(pl.LightningDataModule):
+    def __init__(self,
+                 tokenizer_file: str,
+                 batch_size,
+                 text_max_length: int,
+                 train_file: str = None,
+                 test_file: str = None,
+                 test_sample_size: int = 0
+                 ):
+        super().__init__()
+        self.test_sample_size = test_sample_size
+
+        self.train_file = train_file
+        if train_file:
+            self.train = self.read_files(self.train_file)
+            print("Train Dataset has", len(self.train.data), "rows!")
+
+        self.test_file = test_file
+        if test_file:
+            self.val = self.read_files(self.test_file)
+            print("Val Dataset has", len(self.val.data), "rows!")
+
+        self.tokenizer_file = tokenizer_file
+        self.batch_size = batch_size
+        self.text_max_length = text_max_length
+
+        self.text_tokenizer = PreTrainedTokenizerFast(
+            tokenizer_file=tokenizer_file)
+        self.text_tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+
+    def read_files(self, data_file):
+        df = pd.read_parquet(data_file)
+        df['is_duplicate'] = (df['is_duplicate']).astype(int)
+        return DuplicateDataset(df)
+
+    def encode_texts(self, texts):
+        texts_encoded = self.text_tokenizer.batch_encode_plus(texts)
+        texts_encoded = self.text_tokenizer.pad(
+            texts_encoded, max_length=self.text_max_length, padding='longest')
+
+        texts_masks = torch.tensor(
+            texts_encoded['attention_mask'], dtype=torch.int64)
+        texts_padded = torch.tensor(
+            texts_encoded['input_ids'], dtype=torch.int64)
+        return texts_padded, texts_masks
+
+    def collate_batch(self, batch):
+        post1_titles = []
+        post1_descs = []
+
+        post2_titles = []
+        post2_descs = []
+
+        labels = []
+
+        for post1_x, post2_x, _y in batch:
+            post1_titles.append(post1_x[3])
+            post1_descs.append(post1_x[4])
+
+            post2_titles.append(post2_x[3])
+            post2_descs.append(post2_x[4])
+
+            labels.append(_y)
+
+        # Post1 Encodings
+        post1_titles_padded, post1_titles_masks = self.encode_texts(
+            post1_titles)
+        post1_descs_padded, post1_descs_masks = self.encode_texts(post1_descs)
+        post1_x = [post1_titles_padded, post1_descs_padded]
+
+        # Post2 Encodings
+        post2_titles_padded, post2_titles_masks = self.encode_texts(
+            post2_titles)
+        post2_descs_padded, post2_descs_masks = self.encode_texts(post2_descs)
+        post2_x = [post2_titles_padded, post2_descs_padded]
+
+        labels = torch.tensor(labels, dtype=torch.float)
+
+        return post1_x, post2_x, labels
+
+    def train_dataloader(self):
+        return DataLoader(self.train, batch_size=self.batch_size,
+                          collate_fn=self.collate_batch,
+                          shuffle=True,
+                          num_workers=8)
+
+    def val_dataloader(self):
+        if self.test_sample_size:
+            self.val.set_sample_data(self.test_sample_size)
+
+        return DataLoader(self.val, batch_size=self.batch_size, collate_fn=self.collate_batch, num_workers=8)
---
+++ src/modeling/siamese_bilstm_simple/model.py
@@ -0,0 +1,187 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+from torchmetrics import Accuracy, MetricCollection, AUROC
+
+
+class BiLSTMBlock(nn.Module):
+    def __init__(self,
+                 text_emb_dim,
+                 num_layers):
+        super().__init__()
+
+        self.bilstm_layers = nn.LSTM(
+            text_emb_dim, text_emb_dim, bidirectional=True, num_layers=num_layers)
+
+    def forward(self, text_embed, mask=None):
+        text_embed, _ = self.bilstm_layers(text_embed)
+        text_embed_mean, _ = torch.mean(text_embed, dim=1)
+        return text_embed_mean
+
+
+class DeepBiLSTM(pl.LightningModule):
+    def __init__(self,
+                 text_vocab_size,
+                 text_emb_dim,
+                 text_num_layers,
+                 output_feature_dim,
+                 dropout_rate: float):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.text_embedding = nn.Embedding(text_vocab_size, text_emb_dim)
+
+        self.bilstm = BiLSTMBlock(
+            text_emb_dim=text_emb_dim, num_layers=text_num_layers)
+
+        # Title + Desc
+        concat_layer_dim = (2 * text_emb_dim) + (2 * text_emb_dim)
+        self.dropout_layer = nn.Dropout(dropout_rate)
+
+        self.output_layer = nn.Sequential(    
+            self.dropout_layer,
+            nn.Linear(concat_layer_dim, concat_layer_dim),
+            nn.ReLU(inplace=True),
+            
+            self.dropout_layer,
+            nn.Linear(concat_layer_dim, output_feature_dim),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, title_tokens, desc_tokens):
+        title_embed = self.text_embedding(title_tokens)
+        title_embed_mean = self.bilstm(title_embed)
+
+        desc_embed = self.text_embedding(desc_tokens)
+        desc_embed_mean = self.bilstm(desc_embed)
+
+        concated_features = torch.cat(
+            [title_embed_mean, desc_embed_mean], dim=1)
+
+        # TODO: try tanh, relu, sigmoid, MultiheadAttention, ELU
+        output = torch.tanh(self.output_layer(concated_features))
+
+        return output
+
+
+class DuplicateSiameseBiLSTM(pl.LightningModule):
+    def __init__(self,
+                 text_vocab_size,
+                 text_emb_dim,
+                 title_num_layers,
+                 desc_num_layers,
+                 slug_vocab_size: int,
+                 city_size: int,
+                 neighbor_size: int,
+                 features_emb_dim: int,
+                 output_feature_dim,
+                 dropout_rate: float,
+                 initial_lr):
+        super().__init__()
+        self.save_hyperparameters()
+        self.encoder = DeepBiLSTM(text_vocab_size,
+                                  text_emb_dim,
+                                  title_num_layers,
+                                  desc_num_layers,
+                                  slug_vocab_size,
+                                  city_size,
+                                  neighbor_size,
+                                  features_emb_dim,
+                                  output_feature_dim,
+                                  dropout_rate)
+
+
+        CONCAT_LAYER_SIZE = 2 * output_feature_dim
+        
+        self.initial_lr = initial_lr
+
+        self.dropout_layer = nn.Dropout(dropout_rate)
+
+        self.classification_head = nn.Sequential(    
+            self.dropout_layer,
+            nn.Linear(CONCAT_LAYER_SIZE, CONCAT_LAYER_SIZE),
+            
+            self.dropout_layer,
+            nn.Linear(CONCAT_LAYER_SIZE, 1),
+
+            nn.Sigmoid()
+        )
+
+        metrics = MetricCollection([
+            Accuracy(task="binary"),
+            AUROC(task="binary"),
+        ])
+        self.train_metrics = metrics.clone(prefix='train_')
+        self.valid_metrics = metrics.clone(prefix='val_')
+
+    def _extract_post_data(self, post_x):
+        title_tokens, desc_tokens = post_x
+        features = {
+            "title_tokens": title_tokens,
+            "desc_tokens": desc_tokens,
+        }
+        return features
+
+    def forward(self, post1_x, post2_x):
+
+        post1_input = self._extract_post_data(post1_x)
+        post2_input = self._extract_post_data(post2_x)
+
+        post1_feature = self.encoder(**post1_input)
+        post2_feature = self.encoder(**post2_input)
+
+        concated_features = torch.cat([post1_feature, post2_feature], dim=1)
+        duplicate_score = self.classification_head(concated_features).flatten()
+
+        return duplicate_score
+
+    def training_step(self, batch, batch_idx):
+        post1_x, post2_x, y_duplicate = batch
+
+        duplicate_score = self.forward(
+            post1_x=post1_x, post2_x=post2_x)
+
+        duplicate_loss = F.binary_cross_entropy(duplicate_score, y_duplicate)
+
+        self.log("duplicate_loss", duplicate_loss, on_step=True,
+                 on_epoch=True, prog_bar=True, logger=True)
+
+        if batch_idx % 20 == 0:
+            output = self.train_metrics(duplicate_score, y_duplicate.int())
+            self.log_dict(output, on_step=True, on_epoch=True,
+                          prog_bar=True, logger=True)
+
+        if (batch_idx != 0) and (batch_idx % 1000 == 0):
+            self.lr_schedulers().step()
+            
+        return duplicate_loss
+
+    def validation_step(self, batch, batch_idx):
+        post1_x, post2_x, y_duplicate = batch
+
+        duplicate_score = self.forward(
+            post1_x=post1_x, post2_x=post2_x)
+
+        duplicate_loss = F.binary_cross_entropy(duplicate_score, y_duplicate)
+
+        self.log("val_duplicate_loss", duplicate_loss, on_step=True,
+                 on_epoch=True, prog_bar=True, logger=True)
+
+        if batch_idx % 20 == 0:
+            output = self.train_metrics(duplicate_score, y_duplicate.int())
+            self.log_dict(output, on_step=True, on_epoch=True,
+                          prog_bar=True, logger=True)
+
+        return duplicate_loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.initial_lr)
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer, 1, gamma=0.9, last_epoch=-1, verbose=True)
+
+        return [optimizer], [scheduler]
+
+    def count_parameters(self):
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
